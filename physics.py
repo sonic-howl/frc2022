6fdd1df5a5accd4edff14b89bf4c97eaa405c598
Tyler Raposo <tylerraposo12345@gmail.com>
2022-03-01 22:57:02 -0500

    add Traction Control

--- physics.py
+++ physics.py
@@ -10,12 +10,15 @@ class PhysicsEngine:
         self.physics_controller = physics_controller
         self.drivetrain = drivetrains.MecanumDrivetrain(31 * units.inches, 28 * units.inches, 10 * units.fps)
         self.robot = robot_obj
+        # self.gyro = wpilib.simulation.AnalogGyroSim(0)
 
     def update_sim(self, now, tm_diff):
         lfs = self.robot.lfm.get()
         rfs = self.robot.rfm.get()
         lrs = self.robot.lrm.get()
         rrs = self.robot.rrm.get()
-        speeds = self.drivetrain.calculate(lfs, rfs, lrs, rrs)
-        self.physics_controller.drive(speeds, tm_diff)
+        speeds = self.drivetrain.calculate(lfs, rfs, -lrs, -rrs)
+        pose = self.physics_controller.drive(speeds, tm_diff)
+
+        self.robot.navx.setAngleAdjustment(-pose.rotation().degrees() + 5)
         
--- robot.py
+++ robot.py
@@ -3,6 +3,8 @@ import ctre
 import wpilib.drive
 from networktables import NetworkTables
 from auto_recorder import AutoRecorder
+from wpimath.controller import PIDController
+from navx import AHRS
 
 class Robot(wpilib.TimedRobot):
     def robotInit(self):
@@ -22,12 +24,26 @@ class Robot(wpilib.TimedRobot):
         self.rrm = ctre.WPI_TalonFX(2)
         self.lrm.setInverted(True)
         self.robot_drive = wpilib.drive.MecanumDrive(self.lfm,  self.lrm, self.rfm, self.rrm)
-        self.stick = wpilib.Joystick(0)
+        self.stick = wp.Joystick(0)
+
+        # self.stick
+
+        # make navx thing
+        self.navx = AHRS.create_spi()
 
         # Auto-recorder
         self.autorecorder = AutoRecorder([self.lfm,  self.lrm, self.rfm, self.rrm])
 
+        # SmartDashboard
+        self.sd = NetworkTables.getTable("SmartDashboard")
+
+        # timer
+        self.timer = wp.Timer()
+        self.timer.start()
+
     def robotPeriodic(self):
+        if self.timer.hasPeriodPassed(0.5):
+            self.sd.putNumber("Angle", self.navx.getAngle())
 
         self.maxSpeed = self.smartBoard.getNumber("Max Speed", 1) 
     
@@ -45,6 +61,14 @@ class Robot(wpilib.TimedRobot):
         ySpeed = self.square(self.stick.getY()) * self.maxSpeed
         xSpeed = self.square(self.stick.getX() * -1 ) * self.maxSpeed
         zSpeed = self.square(self.stick.getZ() * -1) * self.maxSpeed
+
+        joystickAngle = self.stick.getDirectionDegrees()
+        gyroAngle = self.navx.getAngle()
+        difference = joystickAngle - gyroAngle
+        difference /= 180
+
+        self.sd.putNumber("Joystick Angle", joystickAngle)
+
         self.robot_drive.driveCartesian(ySpeed, xSpeed, zSpeed, 0)
         self.autorecorder.recordAuto()
 
