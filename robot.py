0119e5e9a7f06109beab14b87475246ac3da2b66
tyler raposo <tylerraposo@gmail.com>
2022-03-08 14:41:07 -0800

    Add Limelight

--- robot.py
+++ robot.py
@@ -1,3 +1,5 @@
+
+from turtle import heading
 import wpilib as wp
 import ctre
 import wpilib.drive
@@ -8,7 +10,7 @@ class Robot(wpilib.TimedRobot):
     def robotInit(self):
 
         # Camera Vision
-        wp.CameraServer.launch()
+        # wp.CameraServer.launch()
         
         self.smartBoard = NetworkTables.getTable("SmartDashboard")
         self.maxSpeed = 1
@@ -22,7 +24,8 @@ class Robot(wpilib.TimedRobot):
         self.rrm = ctre.WPI_TalonFX(2)
         self.lrm.setInverted(True)
         self.robot_drive = wpilib.drive.MecanumDrive(self.lfm,  self.lrm, self.rfm, self.rrm)
-        self.stick = wpilib.Joystick(0)
+        self.stick = wp.Joystick(0)
+        self.table = NetworkTables.getTable("Limelight")
 
         # Auto-recorder
         self.autorecorder = AutoRecorder([self.lfm,  self.lrm, self.rfm, self.rrm])
@@ -42,11 +45,30 @@ class Robot(wpilib.TimedRobot):
         self.robot_drive.setSafetyEnabled(False)
         
     def teleopPeriodic(self):
-        ySpeed = self.square(self.stick.getY()) * self.maxSpeed
-        xSpeed = self.square(self.stick.getX() * -1 ) * self.maxSpeed
-        zSpeed = self.square(self.stick.getZ() * -1) * self.maxSpeed
-        self.robot_drive.driveCartesian(ySpeed, xSpeed, zSpeed) 
-        self.autorecorder.recordAuto()
+        
+        if self.stick.getRawButton(1):
+            self.visionTrack()
+        else:
+            ySpeed = self.square(self.stick.getY()) * self.maxSpeed
+            xSpeed = self.square(self.stick.getX() * -1 ) * self.maxSpeed
+            zSpeed = self.square(self.stick.getZ() * -1) * self.maxSpeed
+            self.robot_drive.driveCartesian(ySpeed, xSpeed, zSpeed) 
+            self.autorecorder.recordAuto()
+            
+
+    def visionTrack(self):
+        tx = self.table.getNumber('tx', 0)
+
+        if tx > 1.0 :
+            self.robot_drive.driveCartesian(0, 0, .25)
+
+        else:
+            self.robot_drive.driveCartesian(0, 0, -.25)
+
+        # ty = self.table.getNumber('ty', 0)
+        # ta = self.table.getNumber('ta', 0)
+        # ts = self.table.getNumber('ts', 0)
+            
 
 if __name__ == '__main__':
-    wpilib.run(Robot)
+    wp.run(Robot)
--- vision.py
+++
@@ -1,7 +0,0 @@
-from networktables import NetworkTable, NetworkTables
-
-table = NetworkTables.getTable("Limelight")
-tx = table.getNumber('tx', None)
-ty = table.getNumber('ty', None)
-ta = table.getNumber('ta', None)
-ts = table.getNumber('ts', None)
