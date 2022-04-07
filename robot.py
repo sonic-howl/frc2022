fd883d5813069a24721965e663d64a2e8b713654
unknown <tylerraposo12345@gmail.com>
2022-04-07 15:53:23 -0400

    Add autonomous

--- robot.py
+++ robot.py
@@ -1,3 +1,4 @@
+import rev
 import wpilib as wp
 import ctre
 import wpilib.drive
@@ -23,8 +24,24 @@ def clamp(y, min, max):
     if y < min:
         return min
     return y
-
-
+#  Pickup System for bot
+class PickUp():
+    def __init__(self):
+        self.raising = ctre.WPI_TalonSRX(5)
+        self.spining = rev.CANSparkMax(12, rev.CANSparkMax.MotorType.kBrushless)
+        # uls = upper lower switch and lls = lower upper switch
+        self.uLs = wp.DigitalInput(0)
+        self.lLs = wp.DigitalInput(1)
+    
+    def Raise(self, speed):
+        if self.uLs.get() or self.lLs.get():
+            self.raising.set(speed)
+        else: 
+            self.raising.set(0)
+    
+    def spin(self, speed):
+        self.spining.set(speed)
+        
 class Robot(wp.TimedRobot):
     def __init__(self):
         self.period = 0.01
@@ -41,14 +58,14 @@ class Robot(wp.TimedRobot):
 
         # Initialized the motors
         self.lfm = ctre.WPI_TalonFX(3)
-        self.rfm = ctre.WPI_TalonFX(5)
+        self.rfm = ctre.WPI_TalonFX(2)
         self.lfm.setInverted(True)
         self.lrm = ctre.WPI_TalonFX(4)
-        self.rrm = ctre.WPI_TalonFX(2)
+        self.rrm = ctre.WPI_TalonFX(1)
         self.lrm.setInverted(True)
         self.robot_drive = wpilib.drive.MecanumDrive(self.lfm,  self.lrm, self.rfm, self.rrm)
         # this motor is for the shooter
-        self.shooter = ctre.WPI_TalonSRX(6)
+        self.shooter = rev.CANSparkMax(11, rev.CANSparkMax.MotorType.kBrushless)
 
         # limelight table
         self.limelight = NetworkTables.getTable("limelight")
@@ -56,6 +73,7 @@ class Robot(wp.TimedRobot):
 
         # self.stick
         self.stick = wp.Joystick(0)
+        self.controller = wp.XboxController(1)
 
         # make navx thing
         self.navx = AHRS.create_spi(update_rate_hz=100)
@@ -70,9 +88,9 @@ class Robot(wp.TimedRobot):
         self.sd = NetworkTables.getTable("SmartDashboard")
 
         # tracking pid controller
-        self.trackingPID = PIDController(0.08, 0, 0.08, self.period)
-        self.trackingRotationPID = PIDController(0.48, 0, 0.035, self.period)
-        self.tractionPID = PIDController(0.015, 0, 0.001, self.period)
+        self.trackingPID = PIDController(0.1, 0, 0.08, self.period)
+        self.trackingRotationPID = PIDController(0.5, 0, 0.035, self.period)
+        self.tractionPID = PIDController(0.02, 0, 0.001, self.period)
         self.tractionPID.enableContinuousInput(-180, 180)
         self.lastAngle = 0
         self.strafeCorrectionPID = PIDController(0.12, 0, 0.012, self.period)
@@ -88,9 +106,10 @@ class Robot(wp.TimedRobot):
         self.smartBoard.putNumber("Autorecord counter", 0)
 
         self.limitSwitch = wp.DigitalInput(9)
+
+        self.pickUp = PickUp()
+
         
-
-
     def robotPeriodic(self):
         if self.timer.hasPeriodPassed(0.5):
             self.sd.putNumber("Angle", self.navx.getYaw())
@@ -116,10 +135,13 @@ class Robot(wp.TimedRobot):
 
     def teleopPeriodic(self):
         # shooting ball
-        if self.stick.getRawButton(2):
-            self.shooter.set(0.9)
-        else:
-            self.shooter.set(0)
+        lTS = -self.controller.getLeftTriggerAxis()
+        rTS = self.controller.getRightTriggerAxis()
+        speed = -(lTS + rTS)
+        self.shooter.set(speed)
+
+        if self.controller.getAButton():
+            self.shooter.set(-0.7)
 
         zRotationCorrection = 0
         ySpeedCorrection = 0
@@ -133,12 +155,12 @@ class Robot(wp.TimedRobot):
         else:
             self.limelight.putNumber("ledMode", 1)
 
-        # ySpeed = square(dz(self.stick.getY())) * self.maxSpeed
-        # xSpeed = square(dz(self.stick.getX()) * -1) * self.maxSpeed
-        # zSpeed = square(dz(self.stick.getZ()) * -1) * self.maxSpeed + zRotationCorrection
-        ySpeed = square(self.stick.getRawAxis(1)) * self.maxSpeed + ySpeedCorrection
-        xSpeed = square(self.stick.getRawAxis(0) * -1 ) * self.maxSpeed
-        zSpeed = square(self.stick.getRawAxis(4) * -1) * self.maxSpeed + zRotationCorrection
+        ySpeed = square(dz(self.stick.getY())) * self.maxSpeed + ySpeedCorrection
+        xSpeed = square(dz(self.stick.getX()) * -1) * self.maxSpeed
+        zSpeed = square(dz(self.stick.getZ()) * -1) * self.maxSpeed + zRotationCorrection
+        # ySpeed = square(self.stick.getRawAxis(1)) * self.maxSpeed + ySpeedCorrection
+        # xSpeed = square(self.stick.getRawAxis(0) * -1 ) * self.maxSpeed
+        # zSpeed = square(self.stick.getRawAxis(4) * -1) * self.maxSpeed + zRotationCorrection
 
         # if self.stick.getRawButton(11):
         #     ySpeed = self.strafeCorrectionPID.calculate(self.navx.getVelocityX(), 0)
@@ -151,10 +173,12 @@ class Robot(wp.TimedRobot):
         else:
             self.lastAngle = self.navx.getYaw()
 
-        self.robot_drive.driveCartesian(ySpeed, xSpeed, zSpeed)
+        self.robot_drive.driveCartesian(ySpeed, xSpeed, zSpeed, self.navx.getAngle())
         # self.robot_drive.driveCartesian(0, 0, 0)
         self.autorecorder.recordAuto()
 
+        self.pickUp.Raise(self.controller.getLeftY())
+        self.pickUp.spin(self.controller.getRightY())
 
     def visionTrack(self):
 
